ff4e24d2a2edb21374b72c9fec59dfcea87dd20b
Harshini-3-11 <harshiniselvarani123@gmail.com>
2026-07-31 22:15:01 +0530

    Time: 1 ms (76.79%) | Memory: 42.5 MB (51.57%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isHappy(int n) {
+        for (int i = 0; i < 100; i++) { 
+            if (n == 1) {
+                return true;
+            }
+            n = sumOfSquares(n);
+        }
+        return false;
+    }
+
+    private int sumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
